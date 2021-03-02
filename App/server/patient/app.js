0a44c807b858a920a3f966e40d5930f75174c6eb
jxl <2267754245@qq.com>
2021-03-03 00:30:32 +0800

    3-3

---
+++ App/server/patient/app.js
@@ -0,0 +1,53 @@
+// app.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
+var path = require('path');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var app  = express();
+
+// configuration ===============================================================
+// connect to our database
+
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({
+
+    extended: true
+
+}));
+app.use(bodyParser.json());
+app.use('/',express.static('/home/songjian/go/src/github.com/hyperledger/MedRec/App/server/public'));
+app.use(favicon(path.join('/home/songjian/go/src/github.com/hyperledger/MedRec/App/server/public/', 'patient', 'favicon.ico')));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+} )); // session secret
+app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(require("express-domain-middleware"));
+
+
+// routes ======================================================================
+app.use('/patient',require('./app/patient.js'));
+app.use('/',require('./app/admin.js'));
+app.use('/hospital',require('./app/hospital.js'));
+// launch ======================================================================
+//Setting server port
+app.listen(8081, function () {
+    console.log("Server has started!!!");
+});
